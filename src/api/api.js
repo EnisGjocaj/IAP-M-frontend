198886919cc771aac317e1c942e382aa2e1e1a20
Enis Gjocaj <97288173+EnisGjocaj@users.noreply.github.com>
2024-09-17 01:54:51 +0200

    added the api and finished

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({ baseURL: 'http://localhost:4000' });
+const axiosInstance = axios.create({ baseURL: 'https://iap-m-api.onrender.com' });
 
 export const fetcher = (url) =>
   axiosInstance.get(url).then((res) => {
